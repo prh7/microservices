8f0723046551cdecd4a228aec8e94327c5fd7560
Prajwal <prajwalhalwai@pm.me>
2022-03-29 12:43:02 +0200

    added console.log

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,7 +6,9 @@ import { natsWrapper } from './natsWrapper';
 import { OrderCreatedListener } from './routes/events/listeners/orderCreatedListener';
 import { OrderCancelledListener } from './routes/events/listeners/orderCancelledListener';
 
-const start = async () => {  
+const start = async () => {
+  console.log('Server is starting to run...');
+  
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error('NATS_CLIENT_ID must be defined');
   }
