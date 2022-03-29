2fd8fd815e5fd8b40b30116dd80494bb391f50df
Prajwal <prajwalhalwai@pm.me>
2022-03-29 12:38:42 +0200

    added a dot

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,11 +5,8 @@ import { natsWrapper } from './natsWrapper';
 
 import { OrderCreatedListener } from './routes/events/listeners/orderCreatedListener';
 import { OrderCancelledListener } from './routes/events/listeners/orderCancelledListener';
-import { OrderStatus } from '@phticketing/common';
 
-const start = async () => {
-  console.log('Starting up....');
-  
+const start = async () => {  
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error('NATS_CLIENT_ID must be defined');
   }
